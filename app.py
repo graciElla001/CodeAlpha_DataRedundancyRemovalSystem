1d051d235e839bcf46032ae5e2c361fbce0b744d
graciElla001 <onwuasoanyaijeoma1@gmail.com>
2026-07-12 23:38:45 +0100

    Create HTML form and receive submitted data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from database import create_database
 
 app = Flask(__name__)
@@ -8,8 +8,34 @@ create_database()
 
 @app.route("/")
 def home():
-    return "Welcome to the CodeAlpha Data Redundancy Removal System!"
+    return render_template("index.html")
 
+@app.route("/add", methods=["POST"])
+def add_record():
+
+    full_name = request.form["full_name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+
+    print("Full Name:", full_name)
+    print("Email:", email)
+    print("Phone:", phone)
+
+    return f"""
+    Record received successfully!
+
+    <br><br>
+
+    Name: {full_name}
+
+    <br>
+
+    Email: {email}
+
+    <br>
+
+    Phone: {phone}
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
